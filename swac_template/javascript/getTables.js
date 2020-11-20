c390c530901911fbb52e97add19406deb1b00e61
jwilczek <jelka.wilczek@fh-bielefeld.de>
2020-11-20 14:26:50 +0100

    load data dynamically

---
+++ swac_template/javascript/getTables.js
@@ -0,0 +1,60 @@
+function addTablebox(){
+    // erstmal OpenWeatherdaten, da die Schnittstelle nicht erreichbar ist
+    let url = "http://api.openweathermap.org/data/2.5/forecast?q=Augsburg,De&APPID=ca9d175f389c46b262b56ef51ef64436"
+    //let url = "http://epigraf01.ad.fh-bielefeld.de:28080/SmartDataPVServe/smartdata/records/tbl_observedobject?storage=smartmonitoring&page=1&order=name&countonly=false&deflatt=false"
+    fetch(url).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        console.log("test")
+        console.log(data["list"])
+        var sections =  document.getElementsByClassName("tablelist");
+        console.log(sections[0])
+        var list = [];
+        list = document.createElement("ul");
+        for(var key in data["list"]) {
+            let newTablebox = document.createElement("ol");
+            let newTablediv = document.createElement("div")
+            let newTabledivleft = document.createElement("div")
+            let newTabledivright = document.createElement("div")
+            let newTablelastChange = document.createElement("p")
+            let newTablelastChangeValue = document.createElement("p")
+            let newTableStroge = document.createElement("p")
+            let newTableStorageValue = document.createElement("p")
+            let newTablecontent = document.createElement("p")
+            let newTableheader = document.createElement("h4")
+            newTablediv.className="uk-box-shadow-small uk-box-shadow-hover-large uk-padding tablebox"
+            newTabledivleft.id = "table-left"
+            newTabledivright.id = "table-right"
+            let newTablecontenttext = document.createTextNode("Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.");
+            let newTablename = document.createTextNode(data["list"][key]["dt"]);
+
+            let newTablelastChangeText = document.createTextNode("Letzte Änderung");
+            let newTablelastChangeValueText = document.createTextNode(data["list"][key]["dt_txt"]);
+            newTablelastChange.appendChild(newTablelastChangeText);
+            newTablelastChangeValue.appendChild(newTablelastChangeValueText);
+
+            let newTableStorageText = document.createTextNode("Speicherplatzbedarf");
+            let newTableStorageValueText = document.createTextNode(data["list"][key]["visibility"]);
+            newTableStroge.appendChild(newTableStorageText);
+            newTableStorageValue.appendChild(newTableStorageValueText);
+
+            newTabledivright.appendChild(newTablelastChange);
+            newTabledivright.appendChild(newTablelastChangeValue);
+            newTabledivright.appendChild(newTableStroge);
+            newTabledivright.appendChild(newTableStorageValue);
+
+            newTablecontent.appendChild(newTablecontenttext)
+            newTablebox.appendChild(newTablediv);
+            newTablediv.appendChild(newTabledivleft)
+            newTablediv.appendChild(newTabledivright)
+            newTabledivleft.appendChild(newTableheader);
+            newTabledivleft.appendChild(newTablecontent);
+            newTableheader.appendChild(newTablename);
+            list.appendChild(newTablebox)
+        }
+        sections[0].appendChild(list);
+    }).catch(function() {
+        console.log("Ein Fehler beim Abruf der Daten ist aufgetreten.");
+    });
+}
