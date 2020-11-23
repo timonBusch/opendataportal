61a7e35f131a4ec349971d41ad56f4ddca9eae5a
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2020-11-23 09:20:02 +0100

    added exampledata.js

---
+++ swac_template/data/exampledata.js
@@ -0,0 +1,28 @@
+var exampledata = [];
+exampledata[0] = {
+    id: 1,
+    name: "Datensatz 1",
+    doubleval: 12.0123,
+    intval: 1234,
+    stringval: 'string',
+    dateval: '29.10.2019T7:46:59',
+    refval: 'ref://exampledata/2'
+};
+exampledata[1] = {
+    id: 2,
+    name: "Datensatz 2",
+    doubleval: 19.0234,
+    intval: 2345,
+    stringval: 'string2',
+    dateval: '29.10.2019T7:47:59',
+    refval: 'ref://exampledata/2'
+};
+exampledata[2] = {
+    id: 3,
+    name: "Datensatz 3",
+    doubleval: 42.0345,
+    intval: 3456,
+    stringval: 'string',
+    dateval: '29.10.2019T7:48:59',
+    refval: 'ref://exampledata/2'
+};
