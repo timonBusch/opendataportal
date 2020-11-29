3d89c9675dcab7f1e17cc660571543672eaa7674
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2020-11-29 15:25:02 +0100

    show some details in tabelle_details.html from example object, get id for dataset from url

--- swac_template/javascript/detailedDataset.js
+++ swac_template/javascript/detailedDataset.js
@@ -1,2 +1,12 @@
 const queryString = window.location.search;
 console.log(queryString)
+
+const urlParams = new URLSearchParams(queryString)
+//records.id(urlParams.get(id))
+
+var records = {"records":[{"datacapture":true,"parent_id":7,"type_id":7,"name":"Sensor fuer Ausrichtungstests","description":"Erfasste Werte eines Sensors, der im Verlauf der Messungen unterschiedlich ausgerichtet wurde, um die Auswirkung der verschiedenen Einstrahlungswinkel experimentell nachzuvollziehen.","id":20,"manualcapture":false}]}
+
+
+
+//fetch('tbl_observedobject.json').then(res => res.json()).then(data => console.log(data))
+
