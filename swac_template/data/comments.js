fe67e84de41a23c983e1c4d8f3e4aab887a3a5a5
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2020-11-29 14:25:09 +0100

    added swac for comments

---
+++ swac_template/data/comments.js
@@ -0,0 +1,32 @@
+var comments = []
+
+comments[0] = {
+    id: 0,
+    content: "Hey diese Daten sind voll cool!",
+    timestamp: new Date(),
+    author: "blal.bla@gmx.de"
+}
+comments[1] = {
+    id: 1,
+    content: "Mh ich denke die Daten könnten cooler sein",
+    timestamp: new Date(),
+    author: "blub.bli@yahoo.de"
+}
+comments[2] = {
+    id: 2,
+    content: "............",
+    timestamp: new Date(),
+    author: "blub.bli@yahoo.de"
+}
+comments[3] = {
+    id: 3,
+    content: "Sehen gut aus!",
+    timestamp: new Date(),
+    author: "blub.bli@yahoo.de"
+}
+comments[4] = {
+    id: 4,
+    content: "Da ist ein Fehler drinne!",
+    timestamp: new Date(),
+    author: "blub.bli@yahoo.de"
+}
