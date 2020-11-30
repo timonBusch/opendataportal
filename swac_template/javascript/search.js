f0dcfa1665fd86a11c9c6fdac641b583f1c91165
Fabian List <fabian.list@fh-bielefeld.de>
2020-11-30 11:03:37 +0100

    fixing search.js for new layout

--- swac_template/javascript/search.js
+++ swac_template/javascript/search.js
@@ -1,26 +1,28 @@
 function searchTable() {
     // Variablendeklarierung
-    var input, filter, ul, ol, a, i, txtValue, countTables=0;
+
+    var input, filter, card, ul, a, i, txtValue, countTables=0;
     input = document.getElementById('searchbar');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("tablelist");
-    ol = ul.getElementsByTagName('ol');
-    if(ol == null){
+    card = document.getElementById("present_example4");
+    ul = card.getElementsByTagName('ul');
+    if(ul == null){
         return
     }
 
     // Tabellentitel nach Suchbegriff durchlaufen und entsprechend ein-/ausblenden
-    for (i = 0; i < ol.length; i++) {
-        a = ol[i].getElementsByTagName("h4")[0];
+    for (i = 0; i < ul.length; i++) {
+        a = ul[i].getElementsByTagName("h4")[0];
         txtValue = a.textContent || a.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            ol[i].style.display = "";
+            ul[i].style.display = "";
             countTables++;
         } else {
-            ol[i].style.display = "none";
+            ul[i].style.display = "none";
         }
     }
     // Anpassen der Trefferzahl
     tableNumber = document.getElementById('tableNumber');
     tableNumber.textContent = "Anzahl der Treffer: " + countTables.toString();
+
 }
