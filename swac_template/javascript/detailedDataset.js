7cf82287a7bd9a574b589673bb4af9312cf13d96
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2020-11-30 11:12:56 +0100

    Dataset number is now shown and data can now be previewed

--- swac_template/javascript/detailedDataset.js
+++ swac_template/javascript/detailedDataset.js
@@ -3,13 +3,24 @@ console.log(queryString)
 
 const urlParams = new URLSearchParams(queryString)
 
-let url = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/tbl_observedobject?storage=smartmonitoring&filter=id%2Ceq%2C" + urlParams.get('id') + "&size=1&page=1&countonly=false&deflatt=false"
+let url_description = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/tbl_observedobject?storage=smartmonitoring&filter=id%2Ceq%2C" + urlParams.get('id') + "&size=1&page=1&countonly=false&deflatt=false"
 
-var records
-
-fetch(url)
+var description_record
+fetch(url_description)
     .then(response => response.json())
-    .then(data => records = data);
+    .then(data => {
+        description_record = data;
 
+    });
 
+let url_dataset = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + urlParams.get('id') + "?storage=smartmonitoring&page=1&countonly=false&deflatt=false"
+
+var dataset
+fetch(url_dataset)
+    .then(response => response.json())
+    .then(data =>{
+        dataset = data
+        let number = document.getElementById("number_dataset");
+        number.textContent = dataset["records"].length + " Datensätze zur verfügung";
+    });
 
