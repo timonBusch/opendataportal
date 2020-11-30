94bc2365291b30ccb0826b49aa1a0bc72e3a7757
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2020-11-30 12:32:01 +0100

    changed counting for dataset

--- swac_template/javascript/detailedDataset.js
+++ swac_template/javascript/detailedDataset.js
@@ -1,5 +1,4 @@
 const queryString = window.location.search;
-console.log(queryString)
 
 const urlParams = new URLSearchParams(queryString)
 
@@ -13,14 +12,23 @@ fetch(url_description)
 
     });
 
-let url_dataset = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + urlParams.get('id') + "?storage=smartmonitoring&page=1&countonly=false&deflatt=false"
+let url_dataset = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + urlParams.get('id') + "?storage=smartmonitoring&page=2&countonly=false&deflatt=false"
 
 var dataset
 fetch(url_dataset)
     .then(response => response.json())
     .then(data =>{
         dataset = data
-        let number = document.getElementById("number_dataset");
-        number.textContent = dataset["records"].length + " Datensätze zur verfügung";
     });
 
+let url_dataset_count = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + urlParams.get('id') + "?storage=smartmonitoring&size=0&countonly=true&deflatt=false"
+
+fetch(url_dataset_count)
+    .then(response => response.json())
+    .then(data =>{
+        let count = data
+        let number = document.getElementById("number_dataset");
+        number.textContent = count.records[0].count + " Datensätze zur verfügung";
+    });
+
+SWA
