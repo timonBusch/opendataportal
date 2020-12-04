02db75c65d96e5a6209ec7db716c41aa30b398e9
jwilczek <jelka.wilczek@fh-bielefeld.de>
2020-12-04 15:15:09 +0100

    added example categories

---
+++ swac_template/data/exampleCategories.js
@@ -0,0 +1,16 @@
+var exampleCategories = [];
+exampleCategories[0] = {
+    id: 1,
+    name: "Photovoltaik",
+    description: "Wann scheint wo die Sonne?"
+};
+exampleCategories[1] = {
+    id: 2,
+    name: "Wetterdatem",
+    description: "Wie ist das Wetter?"
+};
+exampleCategories[2] = {
+    id: 3,
+    name: "Sonstiges",
+    description: "Alles andere"
+};
--- swac_template/javascript/getTables.js
+++ swac_template/javascript/getTables.js
@@ -1,16 +1,30 @@
 let url = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/tbl_observedobject?storage=smartmonitoring&order=DESC&countonly=false&deflatt=false"
+var exampleoptions = {
+    showWhenNoData: false,
+    sortable: true
+};
 
-const promiseOfSomeData =
+const promiseOfSomeJsonData =
     fetch(url)
     .then(r=>r.json())
     .then(data => {
-        fetchedData = data["records"]
+        fetchedData = data["records"].sort(function(a,b){
+            if(a.name == b.name)
+                return 0;
+            if(a.name < b.name)
+                return -1;
+            if(a.name > b.name)
+                return 1;
+        });
         console.log("in async");
         document.getElementById("tableNumber");
         tableNumber.textContent = "Anzahl der Treffer: " + data["records"].length;
         return data;
     });
+
 window.onload = async () => {
     let someData = await promiseOfSomeJsonData;
+    console.log(fetchedData)
     console.log("onload");
+
 };
