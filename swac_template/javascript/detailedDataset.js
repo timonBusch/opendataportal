2dd26c99f330cdcb4a1086bb3b4b7b948d4b4dc7
Timon Buschendorf <timon.buschendorf@fh-bielefeld.de>
2020-12-07 11:50:00 +0100

    adjusted number of dataset and added right attributes to filter the table

--- swac_template/javascript/detailedDataset.js
+++ swac_template/javascript/detailedDataset.js
@@ -1,32 +1,46 @@
+
+window.onload = function () {
+    getData(url_dataset_count).then(data => {
+        let count = data
+        let number = document.getElementById("number_dataset");
+        number.textContent = count.records[0].count + " Datensätze zur Verfügung";
+    })
+}
+
 const queryString = window.location.search;
 
 const urlParams = new URLSearchParams(queryString)
+let id = urlParams.get('id')
 
-let url_description = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/tbl_observedobject?storage=smartmonitoring&filter=id%2Ceq%2C" + urlParams.get('id') + "&size=1&page=1&countonly=false&deflatt=false"
+const url_description = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/tbl_observedobject?storage=smartmonitoring&filter=id%2Ceq%2C" + urlParams.get('id') + "&size=1&page=1&countonly=false&deflatt=false"
+const url_dataset = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + id + "?storage=smartmonitoring&page=2&countonly=false&deflatt=false"
+const url_dataset_count = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + id + "?storage=smartmonitoring&size=0&countonly=true&deflatt=false"
+const url_dataset_keys = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/collection/data_" + id + "/getAttributes?storage=smartmonitoring"
 
-var description_record
-fetch(url_description)
-    .then(response => response.json())
-    .then(data => {
-        description_record = data;
 
-    });
+async function getData(url) {
+    let resonse = await fetch(url);
+    return await resonse.json()
+}
 
-let url_dataset = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + urlParams.get('id') + "?storage=smartmonitoring&page=2&countonly=false&deflatt=false"
+var description_record;
+getData(url_description).then(data => {
+    description_record = data
+})
 
-var dataset
-fetch(url_dataset)
-    .then(response => response.json())
-    .then(data =>{
-        dataset = data
-    });
+var dataset;
+getData(url_dataset).then(data =>{
+    dataset = data
+});
 
-let url_dataset_count = "http://epigraf01.ad.fh-bielefeld.de:8080/SmartDataTeststand/smartdata/records/data_" + urlParams.get('id') + "?storage=smartmonitoring&size=0&countonly=true&deflatt=false"
+var dataset_keys;
+getData(url_dataset_keys).then(data => {
+    dataset_keys = data
+})
 
-fetch(url_dataset_count)
-    .then(response => response.json())
-    .then(data =>{
-        let count = data
-        let number = document.getElementById("number_dataset");
-        number.textContent = count.records[0].count + " Datensätze zur verfügung";
-    });
+
+
+
+
+
+
