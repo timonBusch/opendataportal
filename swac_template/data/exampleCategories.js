4c03f5727e544ca37a7c81c7d8d196f085e0435d
jwilczek <jelka.wilczek@fh-bielefeld.de>
2020-12-11 13:36:57 +0100

    beautified startpage and outline

--- swac_template/data/exampleCategories.js
+++ swac_template/data/exampleCategories.js
@@ -11,6 +11,21 @@ exampleCategories[1] = {
 };
 exampleCategories[2] = {
     id: 3,
+    name: "Astronomiedaten",
+    description: "Wie ist der Weltraum?"
+};
+exampleCategories[3] = {
+    id: 4,
+    name: "Luftfeuchtigkeit",
+    description: "Wie feucht ist die Luft?"
+};
+exampleCategories[4] = {
+    id: 5,
+    name: "Verkehrdaten",
+    description: "Wie ist der Verkehr?"
+};
+exampleCategories[5] = {
+    id: 6,
     name: "Sonstiges",
     description: "Alles andere"
 };
